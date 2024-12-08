aa93b75992b723c5181470d8de63ec545902533e
Alexander Williamson <alex@alexw.co.uk>
2024-12-08 11:34:18 +0000

    04 complete (rough)

--- 2024/04/04.test.ts
+++ 2024/04/04.test.ts
@@ -2,89 +2,309 @@ import * as fs from "fs";
 
 const basic_h = fs.readFileSync("2024/04/basic_h.txt").toString();
 const basic_w = fs.readFileSync("2024/04/basic_w.txt").toString();
-const basic_dot_example = fs.readFileSync("2024/04/basic_dot_example.txt").toString();
+const input_10_lines = fs.readFileSync("2024/04/input_10_lines.txt").toString();
+const input_10_lines_90 = fs.readFileSync("2024/04/input_10_lines_90.txt").toString();
+const input = fs.readFileSync("2024/04/input.txt").toString();
 
-// describe("getStrings", () => {
-//   it("returns all horizontal leftToRight strings", () => {
-//     expect(getStrings(basic_dot_example).leftToRight).toEqual(["..X...", ".SAMX.", ".A..A.", "XMAS.S", ".X...."]);
-//   });
-//   it("returns all rightToLeft strings", () => {
-//     expect(getStrings(basic_dot_example).rightToLeft).toEqual(["...X..", ".XMAS.", ".A..A.", "S.SAMX", "....X."]);
-//   });
-//   it("returns all topToBottom strings", () => {
-//     expect(getStrings(basic_dot_example).topToBottom).toEqual(["...X.", ".SAMX", "XA.A.", ".M.S.", ".XA..", "...S."]);
-//   });
-//   it("returns all bottomToTop strings", () => {
-//     expect(getStrings(basic_dot_example).bottomToTop).toEqual([".X...", "XMAS.", ".A.AX", ".S.M.", "..AX.", ".S..."]);
-//   });
-//   it("returns all diagTopLeftToBottomRight strings", () => {
-//     expect(getStrings(basic_dot_example).diagTopLeftToBottomRight).toEqual([".", "..", ".SX", "XAA.", ".M.M.", "XA.X.", ".SA.", "...", ".S", "."]);
-//   });
-// });
+const LOOK_AHEAD_REGEX = /(?=(XMAS|SAMX))/g;
 
-describe("diagTopLeftToBottomRight", () => {
-  it("returns the right values for a basic dot grid", () => {
-    expect(diagTopLeftToBottomRight(getLines(basic_w))).toEqual(["A", "BF", "CGK", "DHL", "EIM", "JN", "O"]);
+describe("part one", () => {
+  describe("countWords", () => {
+    it("finds XMAS 18 times", () => {
+      const strings = getStrings(getLines(input_10_lines));
+
+      const result = countWords(strings, LOOK_AHEAD_REGEX);
+
+      expect(result).toEqual(18);
+    });
+
+    it("finds XMAS 18 times (90 deg rotated)", () => {
+      const strings = getStrings(getLines(input_10_lines_90));
+
+      const result = countWords(strings, LOOK_AHEAD_REGEX);
+
+      expect(result).toEqual(18);
+    });
+
+    it("solves input", () => {
+      const strings = getStrings(getLines(input));
+
+      const result = countWords(strings, LOOK_AHEAD_REGEX);
+
+      expect(result).toEqual(2517);
+    });
+
+    it("returns the right words for 10x10", () => {
+      const strings = getStrings(getLines(input_10_lines));
+
+      expect(strings).toEqual([
+        "MMMSXXMASM",
+        "MSAMXMSMSA",
+        "AMXSXMAAMM",
+        "MSAMASMSMX",
+        "XMASAMXAMM",
+        "XXAMMXXAMA",
+        "SMSMSASXSS",
+        "SAXAMASAAA",
+        "MAMMMXMMMM",
+        "MXMXAXMASX",
+        "MMAMXXSSMM",
+        "MSMSMXMAAX",
+        "MAXAAASXMM",
+        "SMSMSMMAMX",
+        "XXXAAMSMMA",
+        "XMMSMXAAXX",
+        "MSAMXXSSMM",
+        "AMASAAXAMA",
+        "SSMMMMSAMS",
+        "MAMXMASAMX",
+        "M",
+        "MM",
+        "MSA",
+        "SAMM",
+        "XMXSX",
+        "XXSAMX",
+        "MMXMAXS",
+        "ASMASAMS",
+        "SMASAMSAM",
+        "MSAMMMMXAM",
+        "AMSXXSAMX",
+        "MMAXAMMM",
+        "XMASAMX",
+        "MMXSXA",
+        "ASAMX",
+        "SAMM",
+        "AMA",
+        "MS",
+        "X",
+        "M",
+        "SA",
+        "ASM",
+        "MMMX",
+        "XSAMM",
+        "XMASMA",
+        "SXMMAMS",
+        "MMXSXASA",
+        "MASAMXXAM",
+        "MSXMAXSAMX",
+        "MMASMASMS",
+        "ASAMSAMA",
+        "MMAMMXM",
+        "XXSAMX",
+        "XMXMA",
+        "SAMX",
+        "SAM",
+        "MX",
+        "M",
+      ]);
+    });
+
+    describe("getStrings", () => {
+      it("returns the correct strings for a simple w grid", () => {
+        expect(getStrings(getLines(basic_w))).toEqual([
+          "ABCDE",
+          "FGHIJ",
+          "KLMNO",
+          "AFK",
+          "BGL",
+          "CHM",
+          "DIN",
+          "EJO",
+          "A",
+          "BF",
+          "CGK",
+          "DHL",
+          "EIM",
+          "JN",
+          "O",
+          "E",
+          "DJ",
+          "CIO",
+          "BHN",
+          "AGM",
+          "FL",
+          "K",
+        ]);
+        expect(getStrings(getLines(basic_h))).toEqual([
+          "ABC",
+          "DEF",
+          "GHI",
+          "JKL",
+          "MNO",
+          "ADGJM",
+          "BEHKN",
+          "CFILO",
+          "A",
+          "BD",
+          "CEG",
+          "FHJ",
+          "IKM",
+          "LN",
+          "O",
+          "C",
+          "BF",
+          "AEI",
+          "DHL",
+          "GKO",
+          "JN",
+          "M",
+        ]);
+      });
+    });
   });
-  it("returns the right values for a basic dot grid", () => {
-    expect(diagTopLeftToBottomRight(getLines(basic_h))).toEqual(["A", "BD", "CEG", "FHJ", "IKM", "LN", "O"]);
+
+  describe("down", () => {
+    it("returns down words", () => {
+      expect(down(getLines(basic_w))).toEqual(["AFK", "BGL", "CHM", "DIN", "EJO"]);
+    });
   });
-});
 
-describe("diagTopRightToBottomLeft", () => {
-  it("returns the right values for a basic dot grid", () => {
-    expect(diagTopRightToBottomLeft(getLines(basic_w))).toEqual(["E", "DJ", "CIO", "BHN", "AGM", "FL", "K"]);
+  describe("diagTopLeftToBottomRight", () => {
+    it("returns the right values for a basic dot grid", () => {
+      expect(diagTopLeftToBottomRight(getLines(basic_w))).toEqual(["A", "BF", "CGK", "DHL", "EIM", "JN", "O"]);
+    });
+    it("returns the right values for a basic dot grid", () => {
+      expect(diagTopLeftToBottomRight(getLines(basic_h))).toEqual(["A", "BD", "CEG", "FHJ", "IKM", "LN", "O"]);
+    });
   });
-  it("returns the right values for a basic dot grid", () => {
-    expect(diagTopRightToBottomLeft(getLines(basic_h))).toEqual(["C", "BF", "AEI", "DHL", "GKO", "JN", "M"]);
+
+  describe("diagTopRightToBottomLeft", () => {
+    it("returns the right values for a basic dot grid", () => {
+      expect(diagTopRightToBottomLeft(getLines(basic_w))).toEqual(["E", "DJ", "CIO", "BHN", "AGM", "FL", "K"]);
+    });
+    it("returns the right values for a basic dot grid", () => {
+      expect(diagTopRightToBottomLeft(getLines(basic_h))).toEqual(["C", "BF", "AEI", "DHL", "GKO", "JN", "M"]);
+    });
   });
-});
 
-describe("diagBottomLeftToTopRight", () => {
-  it("returns the right values for a basic dot grid", () => {
-    expect(diagBottomLeftToTopRight(getLines(basic_w))).toEqual(["K", "LF", "MGA", "NHB", "OIC", "JD", "E"]);
+  describe("diagBottomLeftToTopRight", () => {
+    it("returns the right values for a basic dot grid", () => {
+      expect(diagBottomLeftToTopRight(getLines(basic_w))).toEqual(["K", "LF", "MGA", "NHB", "OIC", "JD", "E"]);
+    });
+    it("returns the right values for a basic dot grid", () => {
+      expect(diagBottomLeftToTopRight(getLines(basic_h))).toEqual(["M", "NJ", "OKG", "LHD", "IEA", "FB", "C"]);
+    });
   });
-  it("returns the right values for a basic dot grid", () => {
-    expect(diagBottomLeftToTopRight(getLines(basic_h))).toEqual(["M", "NJ", "OKG", "LHD", "IEA", "FB", "C"]);
+
+  describe("diagBottomRightToTopLeft", () => {
+    it("returns the right values for a basic dot grid", () => {
+      expect(diagBottomRightToTopLeft(getLines(basic_w))).toEqual(["O", "NJ", "MIE", "LHD", "KGC", "FB", "A"]);
+    });
+    it("returns the right values for a basic dot grid", () => {
+      expect(diagBottomRightToTopLeft(getLines(basic_h))).toEqual(["O", "NL", "MKI", "JHF", "GEC", "DB", "A"]);
+    });
   });
-});
 
-function diagBottomLeftToTopRight(lines: string[]): string[] {
-  const transformedLines = [...lines].reverse();
-  return diagTopLeftToBottomRight(transformedLines);
-}
-
-function diagTopLeftToBottomRight(lines: string[]): string[] {
-  let words: string[] = [];
-  let added = lines[0].length + lines.length;
-  for (let iteration = 0; iteration < added - 1; iteration++) {
-    let word = "";
-
-    let firstX = iteration;
-    let firstY = 0;
-
-    let lastX = firstX - (lines.length - 1);
-    let lastY = lines.length - 1;
-
-    let x = firstX;
-    let y = firstY;
-
-    while (x >= lastX && y <= lastY) {
-      if (lines[y] && lines[y][x]) {
-        word += lines[y][x];
-      }
-      x--;
-      y++;
-    }
-    words.push(word);
+  function countWords(input: string[], regex: RegExp): number {
+    const mapped = input.map((x) => [...x.matchAll(regex)]);
+    const filtered = mapped.filter((x) => x.length > 0);
+    const length = filtered.reduce((acc, curr) => {
+      return acc + curr.length;
+    }, 0);
+    return length;
   }
-  return words;
-}
 
-function diagTopRightToBottomLeft(lines: string[]): string[] {
-  const reversedLines = lines.map((line) => line.split("").reverse().join(""));
-  return diagTopLeftToBottomRight(reversedLines);
-}
+  function getStrings(input: string[]): string[] {
+    const leftToRight = input;
+    const topToBottom = down(input);
+    const tlbr = diagTopLeftToBottomRight(input);
+    const trbl = diagTopRightToBottomLeft(input);
+    return [...leftToRight, ...topToBottom, ...tlbr, ...trbl];
+  }
+
+  function down(input: string[]): string[] {
+    const words = [];
+    for (let x = 0; x < input[0].length; x++) {
+      let word = "";
+      for (let y = 0; y < input.length; y++) {
+        word += input[y][x];
+      }
+      words.push(word);
+    }
+    return words;
+  }
+
+  function diagTopRightToBottomLeft(lines: string[]): string[] {
+    const reversedLines = [...lines].map((line) => line.split("").reverse().join(""));
+    return diagTopLeftToBottomRight(reversedLines);
+  }
+
+  function diagBottomLeftToTopRight(lines: string[]): string[] {
+    const transformedLines = [...lines].reverse();
+    return diagTopLeftToBottomRight(transformedLines);
+  }
+
+  function diagBottomRightToTopLeft(lines: string[]): string[] {
+    const transformedLines = [...lines].reverse().map((line) => line.split("").reverse().join(""));
+    return diagTopLeftToBottomRight(transformedLines);
+  }
+
+  function diagTopLeftToBottomRight(lines: string[]): string[] {
+    let words: string[] = [];
+    let added = lines[0].length + lines.length;
+    for (let iteration = 0; iteration < added - 1; iteration++) {
+      let word = "";
+
+      let firstX = iteration;
+      let firstY = 0;
+
+      let lastX = firstX - (lines.length - 1);
+      let lastY = lines.length - 1;
+
+      let x = firstX;
+      let y = firstY;
+
+      while (x >= lastX && y <= lastY) {
+        if (lines[y] && lines[y][x]) {
+          word += lines[y][x];
+        }
+        x--;
+        y++;
+      }
+      words.push(word);
+    }
+    return words;
+  }
+});
+
+describe("part two", () => {
+  describe("xmas cross finder", () => {
+    it("returns the number of crosses for a simple input", () => {
+      const count = getCross(getLines(input_10_lines));
+
+      expect(count).toEqual(9);
+    });
+
+    it("returns the number of crosses for a simple input", () => {
+      const count = getCross(getLines(input));
+
+      expect(count).toEqual(1960);
+    });
+  });
+
+  function getCross(lines: string[]): number {
+    let count = 0;
+    for (let y = 1; y < lines.length - 1; y++) {
+      for (let x = 1; x < lines[0].length - 1; x++) {
+        const current = lines[y][x];
+        if (current === "A") {
+          const tl = lines[y - 1][x - 1];
+          const tr = lines[y - 1][x + 1];
+          const bl = lines[y + 1][x - 1];
+          const br = lines[y + 1][x + 1];
+          const tlbr = `${tl}${current}${br}`;
+          const bltr = `${bl}${current}${tr}`;
+          const tlbrMatch = tlbr === "MAS" || tlbr === "SAM";
+          const bltrMatch = bltr === "MAS" || bltr === "SAM";
+          if (tlbrMatch && bltrMatch) {
+            count = count + 1;
+          }
+        }
+      }
+    }
+    return count;
+  }
+});
 
 function getLines(input: string): string[] {
   return input
@@ -92,45 +312,3 @@ function getLines(input: string): string[] {
     .map((x) => x.trim())
     .filter((x) => x.length > 0);
 }
-
-// function getStrings(input: string): Result {
-//   const lines = input
-//     .split(`\n`)
-//     .map((x) => x.trim())
-//     .filter((x) => x.length > 0);
-
-//   // down
-//   const leftToRight: string[] = [];
-//   for (let i = 0; i < lines.length; i++) {
-//     leftToRight.push(lines[i]);
-//   }
-//   const rightToLeft: string[] = [...leftToRight.map((x) => [...x].reverse().join(""))];
-
-//   // across
-//   const topToBottom: string[] = [];
-//   for (let i = 0; i < lines[0].length; i++) {
-//     let str = "";
-//     for (let j = 0; j < lines.length; j++) {
-//       str += lines[j][i];
-//     }
-//     topToBottom.push(str);
-//   }
-//   const bottomToTop: string[] = [...topToBottom.map((x) => [...x].reverse().join(""))];
-
-//   // diagonal top left to bottom right
-//   const _diagTopLeftToBottomRight = diagTopLeftToBottomRight(lines);
-
-//   // diagonal top right to bottom left
-//   const _diagTopRightToBottomLeft = diagTopRightToBottomLeft(lines);
-
-//   return {
-//     topToBottom,
-//     bottomToTop,
-//     leftToRight,
-//     rightToLeft,
-//     diagBottomLeftToTopRight: _diagTopLeftToBottomRight.reverse(),
-//     diagBottomRightToTopLeft: _diagTopRightToBottomLeft.reverse(),
-//     diagTopLeftToBottomRight: _diagTopLeftToBottomRight,
-//     diagTopRightToBottomLeft: _diagTopRightToBottomLeft,
-//   };
-// }
